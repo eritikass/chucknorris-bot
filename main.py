22bbd6c52e53a1bca24bdd4863a18fb1d495f95e
tootsy42 <tootsy.braun@gmail.com>
2019-11-13 15:54:42 +0200

    chuck norris joke

---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+import time
+
+
+def get_chucknorris_joke(category=""):
+    url = "https://api.chucknorris.io/jokes/random"
+    if category != "":
+        url = url + "?category=" + category
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+def send_joke(joke):
+    # fields = []
+    # for categorie in joke["categories"]:
+    #     print(categorie)
+    #     fields.append(
+    #         {
+    #             "title": "Category",
+    #             "value": categorie,
+    #             "short": True
+    #         },
+    #     )
+    data = {
+        "attachments": [
+            {
+                "mrkdwn_in": ["text"],
+                "color": "#ffc0cb",
+                "author_name": "Chuck Bot",
+                "author_link": "https://chucknorris.com/",
+                "author_icon": "https://cdn.dribbble.com/users/382712/screenshots/4165164/chuck_norris.png",
+                "title": "It's this time of the day",
+                "title_link": "https://chuck-norris-jokes.com",
+                "text": "Prepare your `pants` lads",
+                "fields": [
+                    {
+                        "title": "Joke category",
+                        "value": joke['categories'][0],
+                        "short": False
+                    },
+                    {
+                        "title": "Joke itself",
+                        "value": joke['value'],
+                        "short": True
+                    }
+                ],
+                "thumb_url": "https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S",
+                "footer": "norris",
+                "footer_icon": "http://www.8bit-ninja.de/versinator/images/8bit_chuck_norris_l.gif",
+                "ts": int(time.time())
+            }
+        ]
+    }
+
+
+
+    requests.post('xxxx', json=data)
+
+jk=get_chucknorris_joke('')
+print(jk['value'])
+print(jk['categories'][0])
+send_joke(jk)
