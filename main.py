131c5559720f3f21bdbcbb389a0a682b48a0fb4d
tootsy42 <tootsy.braun@gmail.com>
2019-11-14 22:23:42 +0200

    adding list of pictures for thumbnail

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import requests
 import time
 import os
+import random
 
 SLACK_URL=os.environ['SLACK_URL']
+pictures = [
+            "https://cdn.dribbble.com/users/1590794/screenshots/6365359/dribbble_shot_hd.png",
+            "https://www.clipartmax.com/png/middle/269-2697353_chuck-norris-chuck-norris-birthday-card.png",
+            "https://cdn3.movieweb.com/i/article/fOrOnyvPBPzDYIjPJ9Ww48aqSMOtD0/798:50/Chuck-Norris-Photo-Intimidation-Quebec-City-Police-Department.jpg",
+            "http://ppcorn.com/us/wp-content/uploads/sites/14/2016/01/Chuck-Norris-art-ppcorn.jpg",
+            "https://www.designer-daily.com/wp-content/uploads/2008/03/chuck-norris.jpg",
+            "https://mir-s3-cdn-cf.behance.net/project_modules/fs/c56a9621909647.563097c296e05.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdl2yIc_B9uiMyJe-lfGUWbqpqJI5erKapm1rQTgCehgy_fvYV",
+            "https://images.fineartamerica.com/images-medium-large/chuck-norris-i-counted-to-infinity-twice-yvan-goudard.jpg"
+            ]
 
 def get_chucknorris_joke(category=""):
     url = "https://api.chucknorris.io/jokes/random"
@@ -58,7 +69,8 @@ def send_joke(joke):
                         #"short": True
                     #}
                 #],
-                "thumb_url": "https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S",
+                #"thumb_url": "https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S",
+                "thumb_url":random.choice(pictures),
                 "footer": "norris",
                 "footer_icon": "http://www.8bit-ninja.de/versinator/images/8bit_chuck_norris_l.gif",
                 "ts": int(time.time())
@@ -72,7 +84,8 @@ def send_joke(joke):
 
 
 
-jk=get_chucknorris_joke()
+jk=get_chucknorris_joke('history')
 print(jk['value'])
+print(random.choice(pictures))
 #print(jk['categories'][0])
 send_joke(jk)
